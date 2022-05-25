c812cb72aa927c673c5a149b46b2f405eb493d9f
Murilo <szczymonix.lol@hotmail.com>
2022-05-25 15:29:12 -0300

    feat: add script for partner company and insert physiotherapist

---
+++ partner-comp-link.js
@@ -0,0 +1,9 @@
+components.GerarLink.onClick(function(){
+    let url = window.location.origin.toString();
+    let company_name = document.querySelector('input').value.replaceAll(" ", "_");
+    let company_id = window.location.href.split('/').pop();
+    let queryParam = `partner_company__name=${company_name}&partner_company__id=${company_id}`;
+    url += `/tatodois-pre-triagem?${queryParam}`;
+    document.querySelector("textarea").value = url
+})
+
