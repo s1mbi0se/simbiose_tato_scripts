7fbafbd9fe1647d2584257b8edfee8b8a64a0be6
dudusohne <dudu.hit@gmail.com>
2022-06-01 14:52:13 -0300

    fix scripts again

---
+++ insert-physio.js
@@ -0,0 +1,10 @@
+components.physiotherapist__cpf.onChange(() => {
+		let physiotherapistCpf = components.physiotherapist__cpf.value
+		components.organization__cnpj.value = physiotherapistCpf
+})
+
+components.physiotherapist__first_name.onChange(() => {
+	let physiotherapistName = components.physiotherapist__first_name.value
+	components.organization__name.value = physiotherapistName
+
+})
