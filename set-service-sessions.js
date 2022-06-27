ff484b9f08722dd962b78c0792771a2201dd2ca3
s1mbi0se <hello@simbioseventures.com>
2022-06-27 20:55:38 +0000

    See https://github.com/s1mbi0se/tato-scripts/commit/b7712c82f9a9954b600bc9a4f177b102b42ac335 from refs/pull/36/merge

---
+++ set-service-sessions.js
@@ -0,0 +1,1 @@
+components.service__type.onChange((()=>{"Individual"===components.service__type.value&&(components.service__sessions.value="1")})),components.service__sessions2.onChange((()=>{components.service__sessions.value=components.service__sessions2.value.toString()}));/*! Mon Jun 27 2022 20:55:34 GMT+0000 (Coordinated Universal Time) !*/
