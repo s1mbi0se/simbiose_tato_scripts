cc32336a1508118fc142f5c941d3177fef5a1e1c
s1mbi0se <hello@simbioseventures.com>
2022-07-14 19:45:29 +0000

    See https://github.com/s1mbi0se/tato-scripts/commit/70d1dd51c62266da43eb2a85f238b705283fb7d3 from refs/heads/master

---
+++ hide-calendar-filter.js
@@ -0,0 +1,1 @@
+document.getElementsByClassName("calendar-filter")[0].style.display="none",document.getElementsByClassName("subtitle mb-1")[0].style.display="none",document.getElementsByClassName("breadcrumb-item")[0].style.display="none";/*! Thu Jul 14 2022 19:45:22 GMT+0000 (Coordinated Universal Time) !*/
